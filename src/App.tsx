3a1f6d53f0db534f4c6e32a39983c36bb38277be
Daviduber69 <davhen0108@skola.goteborg.se>
2025-03-16 13:25:00 +0100

    D:/Program/Git/website

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
       <LanguageProvider>
-          <Router basename="/website/">
+          <Router basename="/website">
               <Navbar />
               <Routes>
                   <Route path="/" element={<Home />} />
